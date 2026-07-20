06b20957e4bcd2611244a4e57f4e8b2a4d7c57b2
Campbell-621 <campbell.621@wright.edu>
2026-07-20 17:24:19 -0400

    Update CubeController.cs

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem;
 
 public class CubeController : NetworkBehaviour
 {
+    public GameObject projectile;
     public float velocity = 5f;
     public float lookSensitivity = 0.05f;
     private Vector2 movement;
@@ -12,11 +13,18 @@ public class CubeController : NetworkBehaviour
 
     private void Start()
     {
+        // Protect from seeing other cameras
+        var camera = GetComponentInChildren<Camera>();
+        camera.enabled = IsOwner;
+        
         rigidBody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        // Protect from host control
+        if( !IsOwner) return;
+        
         MoveAndRotate(movement, look.x);
     }
 
@@ -43,13 +51,38 @@ public class CubeController : NetworkBehaviour
         rigidBody.MoveRotation(Quaternion.Euler(rotation));
     }
 
-    [ServerRpc]
+    public void OnThrow(InputValue value)
+    {
+        // Must be owner to throw
+        // Get player Index
+        // Call RequestThrowServerRpc()
+        
+        if( !IsOwner) return;
+        var playerIndex = GetCOmponent<PlayerInput>().playerIndex;
+        RequestThrowSeverRpc(playerIndex);
+    }
+    
+    // Only runs on server
+    [ServerRpc]    // <--- Attribute
     private void RequestThrowServerRpc(int playerIndex)
     {
+        // Instantiate Projectile
+        // Get projectiles Network Object
+        // Spawn network object for everyone
+        // Set player Index to who "Threw" 
+        
+        var instantiatedProjectile = Instantiate(projectile);
+        var networkObject = instantiatedProjectile.GetComponent<NetworkObject>();
+        networkObject.Spawn();
+
+        var networkProjectile = instantiatedProjectile.GetComponent<Projectile>();
+        networkProjectile.playerIndex.value = playerIndex;
     }
 
+    // Only runs on client
     [ClientRpc]
     public void KillPlayerClientRPC(int playerIndex)
     {
+        
     }
 }
